4a5b0b91f612b5e3f0099f836c4b69ad039509b2
jonathan-robertson <jrob356@icloud.com>
2023-06-13 13:06:43 -0500

    reorganize project files

---
+++ src/Patches/GameManager.cs
@@ -0,0 +1,55 @@
+﻿using HarmonyLib;
+using System;
+
+namespace DaysRemaining.Patches
+{
+    [HarmonyPatch(typeof(GameManager), "SetWorldTime")]
+    internal class GameManager_SetWorldTime_Patches
+    {
+        private static readonly ModLog<GameManager_SetWorldTime_Patches> _log = new ModLog<GameManager_SetWorldTime_Patches>();
+
+        public static void Postfix()
+        {
+            try
+            {
+                var players = GameManager.Instance.World.Players.list;
+                for (var i = 0; i < players.Count; i++)
+                {
+                    if (Helpers.TryGetClientInfo(players[i].entityId, out var clientInfo))
+                    {
+                        Helpers.SetExpirationDaysRemaining(clientInfo, players[i]);
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                _log.Error("Postfix", e);
+            }
+        }
+    }
+
+    [HarmonyPatch(typeof(GameManager), "updateTimeOfDay")]
+    internal class GameManager_updateTimeOfDay_Patches
+    {
+        private static readonly ModLog<GameManager_updateTimeOfDay_Patches> _log = new ModLog<GameManager_updateTimeOfDay_Patches>();
+
+        public static void Postfix()
+        {
+            try
+            {
+                var players = GameManager.Instance.World.Players.list;
+                for (var i = 0; i < players.Count; i++)
+                {
+                    if (Helpers.TryGetClientInfo(players[i].entityId, out var clientInfo))
+                    {
+                        Helpers.SetExpirationDaysRemaining(clientInfo, players[i]);
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                _log.Error("Postfix", e);
+            }
+        }
+    }
+}
--- src/Patches/NetPackageTileEntity.cs
+++ src/Patches/NetPackageTileEntity.cs
@@ -1,7 +1,7 @@
 ﻿using HarmonyLib;
 using System;
 
-namespace DaysRemaining
+namespace DaysRemaining.Patches
 {
     [HarmonyPatch(typeof(NetPackageTileEntity), "Setup", typeof(TileEntity), typeof(TileEntity.StreamModeWrite), typeof(byte))]
     internal class NetPackageTileEntity_Setup_Patches
@@ -34,56 +34,6 @@ namespace DaysRemaining
                                 Helpers.SetExpirationDaysRemaining(clientInfo, player);
                             }
                         }
-                    }
-                }
-            }
-            catch (Exception e)
-            {
-                _log.Error("Postfix", e);
-            }
-        }
-    }
-
-    [HarmonyPatch(typeof(GameManager), "SetWorldTime")]
-    internal class GameManager_SetWorldTime_Patches
-    {
-        private static readonly ModLog<GameManager_SetWorldTime_Patches> _log = new ModLog<GameManager_SetWorldTime_Patches>();
-
-        public static void Postfix()
-        {
-            try
-            {
-                var players = GameManager.Instance.World.Players.list;
-                for (var i = 0; i < players.Count; i++)
-                {
-                    if (Helpers.TryGetClientInfo(players[i].entityId, out var clientInfo))
-                    {
-                        Helpers.SetExpirationDaysRemaining(clientInfo, players[i]);
-                    }
-                }
-            }
-            catch (Exception e)
-            {
-                _log.Error("Postfix", e);
-            }
-        }
-    }
-
-    [HarmonyPatch(typeof(GameManager), "updateTimeOfDay")]
-    internal class GameManager_updateTimeOfDay_Patches
-    {
-        private static readonly ModLog<GameManager_updateTimeOfDay_Patches> _log = new ModLog<GameManager_updateTimeOfDay_Patches>();
-
-        public static void Postfix()
-        {
-            try
-            {
-                var players = GameManager.Instance.World.Players.list;
-                for (var i = 0; i < players.Count; i++)
-                {
-                    if (Helpers.TryGetClientInfo(players[i].entityId, out var clientInfo))
-                    {
-                        Helpers.SetExpirationDaysRemaining(clientInfo, players[i]);
                     }
                 }
             }
